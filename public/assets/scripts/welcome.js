f38ecf8ae0b422705f019d2a28d1d8405d3441ef
wesley franca <wesley@fasters.com.br>
2021-05-10 20:37:17 -0300

    alterações WIP

--- public/assets/scripts/welcome.js
+++ public/assets/scripts/welcome.js
@@ -1,50 +1,46 @@
 
-const welcome = document.querySelectorAll("#welcome");
+document.querySelectorAll("#welcome").forEach((page) => {
+    const step1 = page.querySelector('.step_1');
+    const saudation = page.querySelector('.saudation');
+    const client = page.querySelector('.step_2');
 
-if (welcome) {
-    welcome.forEach((page) => {
-        const step1 = page.querySelector('.step_1');
-        const saudation = page.querySelector('.saudation');
-        const client = page.querySelector('.step_2');
+    client.style.display = "none";
 
-        client.style.display = "none";
+    saudation.innerHTML = '';
 
-        saudation.innerHTML = '';
+    window.onload = attendance;
 
-        window.onload = attendance;
+    function attendance() {
 
-        function attendance() {
+        setTimeout(() => {
+            step1.classList.add('open');
+            saudation.innerHTML = 'Seja bem vindo a I love Pães, uma nova experiência em pães caseiros!'
+        }, 1000);
 
-            setTimeout(() => {
-                step1.classList.add('open');
-                saudation.innerHTML = 'Seja bem vindo a I love Pães, uma nova experiência em pães caseiros!'
-            }, 1000);
+        setTimeout(() => {
+            step1.classList.remove('open');
+        }, 7000);
 
-            setTimeout(() => {
-                step1.classList.remove('open');
-            }, 7000);
+        setTimeout(() => {
+            step1.classList.add('open');
+            saudation.innerHTML = 'Pães quentes e fresquinhos... Uma explosão de sabores... '
+        }, 8000);
 
-            setTimeout(() => {
-                step1.classList.add('open');
-                saudation.innerHTML = 'Pães quentes e fresquinhos... Uma explosão de sabores... '
-            }, 8000);
+        setTimeout(() => {
+            step1.classList.remove('open');
+        }, 13000);
 
-            setTimeout(() => {
-                step1.classList.remove('open');
-            }, 13000);
+        setTimeout(() => {
+            step1.classList.add('open');
+            saudation.innerHTML = 'Se puder fique em casa... nós entregamos pra você!'
+        }, 14000);
 
-            setTimeout(() => {
-                step1.classList.add('open');
-                saudation.innerHTML = 'Se puder fique em casa... nós entregamos pra você!'
-            }, 14000);
+        setTimeout(() => {
+            client.style.display = "flex";
+        }, 20000);
+    }
+});
 
-            setTimeout(() => {
-                client.style.display = "flex";
-            }, 20000);
-
-        }
-    });
-}
 
 
 
